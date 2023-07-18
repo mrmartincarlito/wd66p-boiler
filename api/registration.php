8b48319ddf0f1b921748b375df3e2b491f7bbb5c
jhomar0021 <128261875+jhomar0021@users.noreply.github.com>
2023-07-18 20:14:28 +0800

    Create Filter for Unique Username
    informs user if username already exist in database(avoid js error unique id)

--- api/registration.php
+++ api/registration.php
@@ -10,16 +10,28 @@ if (isset($_POST['register'])) {
     } else {
         $password = password_hash($registerRequest->password, PASSWORD_DEFAULT);
 
-        $sql = "INSERT INTO `tbl_users`(`fname`, `lname`, `course_id`, `username`, `password`, `is_active`) 
-        VALUES ('{$registerRequest->fname}','{$registerRequest->lname}','1','{$registerRequest->username}','{$password}','1')";
+        $sqlDuplicate = "SELECT * FROM `tbl_users` WHERE username = '$registerRequest->username';";
+        $results = $connection->query($sqlDuplicate);
 
-        $isInserted = $connection->query($sql);
 
-        if ($isInserted) {
-            $response = createResponse(200, "Successful", "Successfully Saved");
-        } else {
-            $response = createResponse(300, "Error", "Error while saving user");
+        $count = $results->num_rows;
+
+        if($count == 0){
+            $sql = "INSERT INTO `tbl_users`(`fname`, `lname`, `account_level`, `username`, `password`, `is_active`) 
+            VALUES ('{$registerRequest->fname}','{$registerRequest->lname}','1','{$registerRequest->username}','{$password}','1')";
+    
+            $isInserted = $connection->query($sql);
+    
+            if ($isInserted) {
+                $response = createResponse(200, "Successful", "Successfully Saved");
+            } else {
+                $response = createResponse(300, "Error", "Error while saving user");
+            }
         }
+        else{
+            $response = createResponse(300, "Error", "Username already exist");
+        }
+
     }
 
     
